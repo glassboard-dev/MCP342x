b5d358b19aa71236783f568ddbc8878bcffc5af3
Stephen Murphy <stephenmurphy@glassboard.com>
2021-04-27 12:37:54 -0400

    Merging unit tests and CI into Master (#17)
    * Update README.md
    
    * Adding initial project structure (#2)
    
    * Adding initial project structure
    * Adding template main and filling out readme
    * Adding missing code block closing syntax in the README"
    * Add bounds checking for the dev config
    
    * Fixed bug where results were not properly stored in the result array (#10)
    
    * Fixing comparison bugs in if statements (#12)
    
    * Adding javadoc documentation throughout (#9)
    
    * Removing git diff lines
    
    * Adding Release workflow back in
    
    * Added more unit tests to achieve 100% testing coverage as well as CodeCov CI integration
    
    * Updating gcov artifact directory
    
    * Explicitly pointing to report.xml
    
    * Changing report path
    
    * Fixing typo in report name
    
    * Adding new Cobertura xml report
    
    * Add code coverage badge

--- test/test_mcp342x.c
+++ test/test_mcp342x.c
@@ -1,5 +1,5 @@
+#include <string.h>
 #include "unity.h"
-
 #include "mcp342x.h"
 
 mcp342x_dev_t mcp342x_device;
@@ -8,8 +8,13 @@ int8_t usr_i2c_write(const uint8_t busAddr, const uint8_t *data, const uint32_t 
 int8_t usr_i2c_read(const uint8_t busAddr, uint8_t *data, const uint32_t len);
 void usr_delay_us(uint32_t period);
 
+mcp342x_return_code_t desired_read_ret = MCP342x_RET_OK;
+
 void setUp(void)
 {
+    // Set our desired read return result
+    desired_read_ret = MCP342x_RET_OK;
+
     // Provide the hardware abstraction functions for
     // I2c Read/Write and a micro-second delay function
     mcp342x_device.intf.i2c_addr = 0x6E;
@@ -37,15 +42,46 @@ int8_t usr_i2c_write(const uint8_t busAddr, const uint8_t *data, const uint32_t 
 
 int8_t usr_i2c_read(const uint8_t busAddr, uint8_t *data, const uint32_t len) {
     mcp342x_return_code_t ret = MCP342x_RET_OK;
+    mcp342x_registers_t tmpReg = {0};
 
-    // Received the specified amount of data from the device and store it
-    // in the data buffer
+    switch( desired_read_ret ) {
+        case MCP342x_RET_OK:
+            // A valid i2c read would see the ready bit change to zero.. Go ahead and mask the incoming data
+            // as our register bit map and set the ready bit
+            memcpy(&tmpReg, data, sizeof(tmpReg));
+            tmpReg.bits.config.bits.ready = 0x00;
+            memcpy(data, &tmpReg, sizeof(tmpReg));
+            ret = MCP342x_RET_OK;
+            break;
+
+        case MCP342x_RET_TIMEOUT:
+            // A timeout would see a RET OK on the read,
+            // but the READY bit would just never get set..
+            // So just return OK but don't set ready bit.
+            ret = MCP342x_RET_OK;
+            break;
+
+        case MCP342x_RET_ERROR:
+            ret = MCP342x_RET_ERROR;
+            break;
+
+        default:
+            break;
+    }
 
     return ret;
 }
 
 void usr_delay_us(uint32_t period) {
     // Delay for the requested period
+}
+
+/**************** WRITE CONFIG *************************************/
+void test_mcp342x_writeConfig_AllValid(void)
+{
+    mcp342x_return_code_t ret = mcp342x_writeConfig(&mcp342x_device);
+
+    TEST_ASSERT_EQUAL_INT(MCP342x_RET_OK, ret);
 }
 
 void test_mcp342x_writeConfig_NullDevice(void)
@@ -63,6 +99,44 @@ void test_mcp342x_writeConfig_NullWriteIntf(void)
     ret = mcp342x_writeConfig(&mcp342x_device);
 
     TEST_ASSERT_EQUAL_INT(MCP342x_RET_NULL_PTR, ret);
+}
+
+/**************** SAMPLE CHANNEL *************************************/
+void test_mcp342x_sampleChannel_AllValid_SingleChannel(void)
+{
+    mcp342x_return_code_t ret = MCP342x_RET_OK;
+
+    ret = mcp342x_sampleChannel(&mcp342x_device, 0);
+
+    TEST_ASSERT_EQUAL_INT(MCP342x_RET_OK, ret);
+}
+
+void test_mcp342x_sampleChannel_AllValid_MultiChannel(void)
+{
+    mcp342x_return_code_t ret = MCP342x_RET_OK;
+
+    for(uint8_t i = 0; i < MCP342x_CH__MAX__; i++) {
+        ret = mcp342x_sampleChannel(&mcp342x_device, i);
+        TEST_ASSERT_EQUAL_INT(MCP342x_RET_OK, ret);
+    }
+}
+
+void test_mcp342x_sampleChannel_ReadTimeout(void)
+{
+    mcp342x_return_code_t ret = MCP342x_RET_OK;
+    desired_read_ret = MCP342x_RET_TIMEOUT;
+
+    ret = mcp342x_sampleChannel(&mcp342x_device, 0);
+    TEST_ASSERT_EQUAL_INT(MCP342x_RET_TIMEOUT, ret);
+}
+
+void test_mcp342x_sampleChannel_GenericReadError(void)
+{
+    mcp342x_return_code_t ret = MCP342x_RET_OK;
+    desired_read_ret = MCP342x_RET_ERROR;
+
+    ret = mcp342x_sampleChannel(&mcp342x_device, 0);
+    TEST_ASSERT_EQUAL_INT(MCP342x_RET_ERROR, ret);
 }
 
 void test_mcp342x_sampleChannel_NullDevice(void)
